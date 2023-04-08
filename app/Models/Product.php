7d8ca2f8f4288a5f341a27f8db3e033a5a80d2d7
PiotrZapolski <zapol1230@gmail.com>
2023-04-08 14:05:23 +0200

    Changed migration, added mutator to store product price as cents, created factory and seeder for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,4 +20,17 @@ class Product extends Model
         'description',
         'price',
     ];
+
+    /**
+     * Mutator for storing price in db as cents value and displaying as dollars value.
+     *
+     * return Attribute
+     */
+    protected function price(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value/100,
+            set: fn ($value) => $value*100,
+        );
+    }
 }
